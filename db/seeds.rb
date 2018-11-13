8eb888cfa5bb0bf308e99f5147e9d78f438b6ecc
Adriano Antonucci <adriano@prestige.com.br>
2018-11-13 14:35:20 -0200

    Add autentication from devise

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 
 # Create Users
-User.create name: 'Hellen Antonucci', status: :active, kind: :salesman
-User.create name: 'Ana Julia Antonucci', status: :active, kind: :salesman
-User.create name: 'Adriano Antonuccu', status: :active, kind: :manager
+User.create name: 'Hellen Antonucci', status: :active, kind: :salesman, email: 'hellen@teste.com', password: 123456
+User.create name: 'Ana Julia Antonucci', status: :active, kind: :salesman, email: 'ana@teste.com', password: 123456
+User.create name: 'Adriano Antonuccu', status: :active, kind: :manager, email: 'adriano@teste.com', password: 123456
 #Create products
 Product.create name: 'Smartphone', description:'Um smartphone novo ...', status: :active
 Product.create name: 'Tablet', description:'Um tablet novo ...', status: :active
